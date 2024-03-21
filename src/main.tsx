36cd142322a5a9ac115f198f131289017e96a824
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2024-03-21 10:59:51 -0300

    feat: toast css

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './css/index.css'
+import 'react-toastify/dist/ReactToastify.css';
 
 import { MetaMaskProvider } from "@metamask/sdk-react";
 
