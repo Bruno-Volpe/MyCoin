2c1b39b8f79fd90da8002c58f8d8a685913c4dc0
Bruno Volpe <bruno@Brunos-MacBook-Pro.local>
2024-03-21 10:20:36 -0300

    feat: icons

--- src/components/MetaMask.tsx
+++ src/components/MetaMask.tsx
@@ -1,4 +1,5 @@
 import { useSDK } from "@metamask/sdk-react";
+import { FaEthereum, FaCircle } from "react-icons/fa";
 
 const App = () => {
     const { sdk, connected, balance } = useSDK();
@@ -17,9 +18,10 @@ const App = () => {
     return (
         <div>
             {connected ? (
-                <div className="text-white-100 flex items-center">
+                <div className="text-white-100 flex items-center justify-center">
+                    <FaCircle className="mr-2 text-green-500" /> 
                     <p>Conectado</p>
-                    <p className="text-tertiary ml-4">{balance} ETH</p>
+                    <p className="text-tertiary ml-4 flex items-center justify-center"><FaEthereum /> {balance ?? 0} ETH</p>
                 </div>
             ) : (
                 <button className="text-white-100 hover:text-tertiary" onClick={connect}>Wallet</button>
